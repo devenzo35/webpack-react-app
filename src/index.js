65a0c5bc020a29fd6b49c92f4b9f57ef40f1b169
Enzo <enzocuellar12@gmail.com>
2021-09-21 16:50:03 -0300

    configuring styles

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import react from "react";
 import reactDom from "react-dom";
 import App from "./App";
+import "../styles/index.css";
 
 reactDom.render(<App />, document.getElementById("root"));
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,23 @@
 const path = require("path");
 
+const rulesForJS = {
+  test: /\.js$/,
+  loader: "babel-loader",
+  options: {
+    presets: [["@babel/preset-react", { runtime: "automatic" }]],
+  },
+};
+
+const rulesForCSS = {
+  test: /\.css$/,
+  use: ["style-loader", "css-loader"],
+};
+
 module.exports = {
   output: {
     path: path.resolve(__dirname, "build"),
   },
   module: {
-    rules: [
-      {
-        test: /\.js$/,
-        loader: "babel-loader",
-        options: {
-          presets: ["@babel/preset-react"],
-        },
-      },
-    ],
+    rules: [rulesForJS, rulesForCSS],
   },
 };
